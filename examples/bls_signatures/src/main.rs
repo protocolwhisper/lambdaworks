50c8b7d6777d373bc02278e0724ab7fb05b098b2
Theprotocolwhisperer <chrismata10@gmail.com>
2023-11-15 18:07:07 -0500

    (feature): bls12381 privatekey for signatures

--- examples/bls_signatures/src/main.rs
+++ examples/bls_signatures/src/main.rs
@@ -1,21 +1,24 @@
 use lambdaworks_math::{
-    elliptic_curve::
-        traits::{IsEllipticCurve, IsPairing},
     cyclic_group::IsGroup,
-    elliptic_curve::
-        short_weierstrass::{
-            curves::bls12_381::{
-                pairing::BLS12381AtePairing,
-                curve::BLS12381Curve,
-                twist::BLS12381TwistCurve},
-                point::ShortWeierstrassProjectivePoint},
+    elliptic_curve::short_weierstrass::{
+        curves::bls12_381::{
+            curve::BLS12381Curve, pairing::BLS12381AtePairing, twist::BLS12381TwistCurve,
+        },
+        point::ShortWeierstrassProjectivePoint,
+    },
+    elliptic_curve::traits::{IsEllipticCurve, IsPairing},
     field::{
-        fields::montgomery_backed_prime_fields::{U384PrimeField, IsModulus},
-        element::FieldElement},
+        element::FieldElement,
+        fields::montgomery_backed_prime_fields::{IsModulus, U384PrimeField},
+    },
     unsigned_integer::element::{UnsignedInteger, U384},
-    };
-
-use sha3::{Shake128, digest::{Update, ExtendableOutput, XofReader}};
+};
+use num_bigint::{BigUint, RandBigInt};
+use num_traits::Num;
+use sha3::{
+    digest::{ExtendableOutput, Update, XofReader},
+    Shake128,
+};
 
 pub type G2Point = <BLS12381TwistCurve as IsEllipticCurve>::PointRepresentation;
 
@@ -29,23 +32,42 @@ impl IsModulus<U384> for U384ModulusP {
 type U384FP = U384PrimeField<U384ModulusP>;
 type U384FPElement = FieldElement<U384FP>;
 
-pub fn secretkey_g1 () -> U384FPElement {
+fn generate_privatekey() -> U384FPElement {
+    let max_value_str = "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787"; // Base field r
+    let max_value = BigUint::from_str_radix(max_value_str, 10).unwrap();
 
-    // Private Key:
-    let sk: U384FPElement = U384FPElement::new(UnsignedInteger::from_hex_unchecked("45")); // It must be fixed
+    let mut rng = rand::thread_rng();
+
+    // Calculate the number of bits in the maximum value
+    let max_bits = max_value.bits(); //381
+    let mut random_biguint;
+
+    loop {
+        // Generate a random BigUint with a number of bits up to max_bits
+        random_biguint = rng.gen_biguint(max_bits);
+
+        // Check if the generated number is within the desired range
+        if random_biguint <= max_value {
+            break;
+        }
+    }
+    let sk: U384FPElement = U384FPElement::new(UnsignedInteger::from_hex_unchecked(
+        &random_biguint.to_str_radix(16),
+    )); // // It must be fixed
     sk
 }
 
-pub fn publickey_g1 (secret: U384FPElement) -> ShortWeierstrassProjectivePoint<BLS12381Curve> {
-
+pub fn publickey_g1(secret: U384FPElement) -> ShortWeierstrassProjectivePoint<BLS12381Curve> {
     let _g_1: ShortWeierstrassProjectivePoint<BLS12381Curve> = BLS12381Curve::generator();
 
     // Public Key:
-    let pk: ShortWeierstrassProjectivePoint<BLS12381Curve> = _g_1.operate_with_self(secret.representative());
+    let pk: ShortWeierstrassProjectivePoint<BLS12381Curve> =
+        _g_1.operate_with_self(secret.representative());
     pk
 }
 
-pub fn hash(msg: &str) -> ShortWeierstrassProjectivePoint<BLS12381TwistCurve> { // This function must be fixed
+pub fn hash(msg: &str) -> ShortWeierstrassProjectivePoint<BLS12381TwistCurve> {
+    // This function must be fixed
     let _g_2: ShortWeierstrassProjectivePoint<BLS12381TwistCurve> = BLS12381TwistCurve::generator();
 
     // Message:
@@ -61,18 +83,26 @@ pub fn hash(msg: &str) -> ShortWeierstrassProjectivePoint<BLS12381TwistCurve> { 
         final_hash |= *byte as u128;
     }
 
-    let hash: ShortWeierstrassProjectivePoint<BLS12381TwistCurve> = _g_2.operate_with_self(final_hash);
+    let hash: ShortWeierstrassProjectivePoint<BLS12381TwistCurve> =
+        _g_2.operate_with_self(final_hash);
     hash
 }
 
-pub fn signer_bls (hash: ShortWeierstrassProjectivePoint<BLS12381TwistCurve>, sk: U384FPElement) -> ShortWeierstrassProjectivePoint<BLS12381TwistCurve> {    
-
+pub fn signer_bls(
+    hash: ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+    sk: U384FPElement,
+) -> ShortWeierstrassProjectivePoint<BLS12381TwistCurve> {
     // Signature:
-    let _signature: ShortWeierstrassProjectivePoint<BLS12381TwistCurve> = hash.operate_with_self(sk.representative());
+    let _signature: ShortWeierstrassProjectivePoint<BLS12381TwistCurve> =
+        hash.operate_with_self(sk.representative());
     _signature
 }
 
-pub fn verifying_bls (_signature: ShortWeierstrassProjectivePoint<BLS12381TwistCurve>, pk: ShortWeierstrassProjectivePoint<BLS12381Curve>, hash: ShortWeierstrassProjectivePoint<BLS12381TwistCurve>) -> bool {
+pub fn verifying_bls(
+    _signature: ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+    pk: ShortWeierstrassProjectivePoint<BLS12381Curve>,
+    hash: ShortWeierstrassProjectivePoint<BLS12381TwistCurve>,
+) -> bool {
     let _g_1: ShortWeierstrassProjectivePoint<BLS12381Curve> = BLS12381Curve::generator();
     let p_1 = <BLS12381AtePairing as IsPairing>::compute(&_g_1, &_signature);
     let p_2 = <BLS12381AtePairing as IsPairing>::compute(&pk, &hash);
@@ -81,17 +111,17 @@ pub fn verifying_bls (_signature: ShortWeierstrassProjectivePoint<BLS12381TwistC
 
 #[cfg(test)]
 mod tests {
-    
-    use crate::secretkey_g1;
-    use crate::publickey_g1;
+
+    use crate::generate_privatekey;
     use crate::hash;
+    use crate::publickey_g1;
     use crate::signer_bls;
     use crate::verifying_bls;
 
     #[test]
     fn verify_test_1() {
         let msg = "abc";
-        let secret = secretkey_g1();
+        let secret = generate_privatekey();
         let public = publickey_g1(secret.clone());
         let hash = hash(msg);
         let signer = signer_bls(hash.clone(), secret);
@@ -102,7 +132,7 @@ mod tests {
     #[test]
     fn verify_test_2() {
         let msg = "HelloWorld!";
-        let secret = secretkey_g1();
+        let secret = generate_privatekey();
         let public = publickey_g1(secret.clone());
         let hash = hash(msg);
         let signer = signer_bls(hash.clone(), secret);
